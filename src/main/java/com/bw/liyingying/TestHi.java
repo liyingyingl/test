b8554831578880929471a67d8305ba01612724e6
liyingyingl <1872757780@qq.com>
2019-11-08 10:02:28 +0800

    想知道世界上最美，最可爱的人是谁，请点击

--- src/main/java/com/bw/liyingying/TestHi.java
+++ src/main/java/com/bw/liyingying/TestHi.java
@@ -3,6 +3,12 @@ package com.bw.liyingying;
 public class TestHi {
 	public static void main(String[] args) {
 		System.out.println("lyy,你真美");
+		
+		
+		System.out.println("你是世界上最可爱的");
 	}
+	
+	
 
 }
+
